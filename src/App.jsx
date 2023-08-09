39bcdb88e49ac10d81d1c8e12ff93d8313c2e9a3
Damola <122807356+Damola10@users.noreply.github.com>
2023-08-09 15:59:25 +0100

    mistakenly tampered with App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Pages
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
-      <Routes>
-        
-      </Routes>
+      <Routes></Routes>
     </Router>
-    
-  )
+  );
 }
 
-export default App
+export default App;
