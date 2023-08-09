94c0bcad4ca8e60482602247cc0eb10f89f673a3
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-09 18:22:04 +0100

    imported Link from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 //Pages
 import GamePinPage from "./pages/GamePage/GamePinPage";
