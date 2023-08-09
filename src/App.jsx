499e051123fcf0861bfe3bfe7830fbf96855ae8b
Tolexy95 <toyin95.olubayo@gmail.com>
2023-08-09 11:22:57 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import InputComponent from './components/InputComponent'
+import GamePinPage from './pages/GamePage/GamePinPage'
 
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
