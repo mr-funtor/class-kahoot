a376c08492899bba4a578c735afc8a4e6ac481fe
Okunlola Akinwale <akinwaleokunlola@gmail.com>
2023-08-09 15:19:00 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,9 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //Pages
-<<<<<<< HEAD
 import GamePinPage from './pages/GamePage/GamePinPage'
 import NicknamePage from './pages/Nickname/NicknamePage'
 import WelcomePage from './pages/WelcomePage/WelcomePage'
-=======
-import GamePinPage from "./pages/GamePage/GamePinPage";
-import NicknamePage from "./pages/Nickname/NicknamePage";
-import WelcomePage from "./pages/HomePage/HomePage";
-import CustomInput from "./components/CustomInput/CustomInput";
-
->>>>>>> 611c0603e1871b10b8b5e92cf916c6b75955fc44
 
 function App() {
   return (
