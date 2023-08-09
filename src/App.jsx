e83c4c1c08a63d050fbb860da5f6d0605dabe73d
Tolexy95 <toyin95.olubayo@gmail.com>
2023-08-09 15:11:01 +0100

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 //Pages
 import GamePinPage from "./pages/GamePage/GamePinPage";
 import NicknamePage from "./pages/Nickname/NicknamePage";
-import WelcomePage from "./pages/HomePage/HomePage";
 import CustomInput from "./components/CustomInput/CustomInput";
 
 
@@ -14,7 +14,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
         <Route path="/gamePin" element={<GamePinPage />} />
         <Route path="/nickName" element={<NicknamePage />} />
          </Routes>
