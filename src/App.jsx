ebcfe4a978876c18a16ddcc501c7de492b7f3bc0
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-12 08:33:02 +0100

    bar

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import PlayerWaiting from "./pages/PlayerWaiting";
 
 import CustomInput from "./components/CustomInput/CustomInput";
 import Home from "./pages/homePage/Home";
-import PlayerWaiting from "./pages/PlayerWaiting";
 import Home from "./pages/homePage/Home";
 
 
