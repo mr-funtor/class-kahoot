802014fe462de0ece6941f4811bd08e37945eddd
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-12 17:48:25 +0100

    css reworks

--- src/components/PopUpModal/index.jsx
+++ src/components/PopUpModal/index.jsx
@@ -7,67 +7,60 @@ import { ModalContext } from "../../context/ModalContext";
 import { useContext } from "react";
 
 const PopUpModal = ({ setIsOpen }) => {
-  
   // imports from modal context
-  const { avatars, selectedImageIndex, handleImageSelection } = useContext(ModalContext);
-
+  const { avatars, selectedImageIndex, handleImageSelection } =
+    useContext(ModalContext);
 
   return (
     <>
-      <div className={ styles.darkBg } onClick={ () => setIsOpen(false) } />
-      <div className={ styles.centered }>
-        <div className={ styles.modal }>
-          <div className={ styles.modalHeader }>
-            <h5 className={ styles.heading }>Select an avatar</h5>
+      <div className={styles.darkBg} onClick={() => setIsOpen(false)} />
+      <div className={styles.centered}>
+        <div className={styles.modal}>
+          <div className={styles.modalHeader}>
+            <h5 className={styles.heading}>Select an avatar</h5>
           </div>
 
-          {/* // modal and it's contents */ }
+          {/* // modal and it's contents */}
 
-          {/* the X icon on the opened modal. It closes the modal */ }
-          <button className={ styles.closeBtn } onClick={ () => setIsOpen(false) }>
-            <RiCloseLine style={ { marginBottom: "-3px" } } />
+          {/* the X icon on the opened modal. It closes the modal */}
+          <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
           </button>
-          {/* display images */ }
-          <div className={ styles.modalContent }>
-            <div style={ { display: "flex" } }>
-              <div
-                style={ {
-                  display: "grid",
-                  gridTemplateColumns: "repeat(4, 1fr)",
-                  gap: "1em",
-                } }
-              >
-                { avatars.map((image, index) => (
+          {/* display images */}
+          <div className={styles.modalContent}>
+            <div>
+              <div className={styles.modalContentWrap}>
+                {avatars.map((image, index) => (
                   <div
-                    key={ index }
-                    style={ {
+                    key={index}
+                    style={{
                       width: "80px",
                       borderRadius: "100%",
-                    } }
+                    }}
                   >
                     <img
-                      style={ {
+                      style={{
                         width: "80px",
                         borderRadius: "100%",
                         border:
                           selectedImageIndex === index
                             ? "3px solid #0e0132"
                             : "none",
-                      } }
-                      src={ image.imageSource }
-                      alt={ `Image ${index}` }
-                      onClick={ () => handleImageSelection(index) }
+                      }}
+                      src={image.imageSource}
+                      alt={`Image ${index}`}
+                      onClick={() => handleImageSelection(index)}
                     />
                   </div>
-                )) }
+                ))}
               </div>
             </div>
           </div>
-          {/* select button */ }
-          <div className={ styles.modalActions }>
+          {/* select button */}
+          <div className={styles.modalActions}>
             <button
-              className={ styles.selectBtn }
-              onClick={ () => setIsOpen(false) }
+              className={styles.selectBtn}
+              onClick={() => setIsOpen(false)}
             >
               Select
             </button>
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,53 +1,51 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import style from "./Homepage.module.css";
-import { logo, icon, iconTwo } from '../../assets';
+import { logo, icon, iconTwo } from "../../assets";
 
 const Home = () => {
-    let cardItems = [
-        {
-            icon: logo,
-            image: icon,
-            user: "Host",
-            text: "Create a room",
-            linkto: '/createRoom'
-        },
-        {
-            icon: logo,
-            image: iconTwo,
-            user: "Join",
-            text: "Join a room",
-            linkto: '/gamePin'
-        }
-    ]
+  let cardItems = [
+    {
+      icon: logo,
+      image: icon,
+      user: "Host",
+      text: "Create a room",
+      linkto: "/createRoom",
+    },
+    {
+      icon: logo,
+      image: iconTwo,
+      user: "Join",
+      text: "Join a room",
+      linkto: "/gamePin",
+    },
+  ];
 
-    return (
-        <>
-            <div className={ style.home }>
-                <h1>Welcome To Clahoot</h1>
-                <div className={ style.cardDiv }>
-                    { cardItems.map((cards, index) => {
-                        return (
-                            <div
-                                className={ style.theCards }
-                                key={ index }
-                                { ...cards }
-                            >
-                                {/* <div className={style.iconDiv}><img src={cards.icon} alt="" /></div> */ }
-                                <div className={ style.iconDiv }> <div className={ style.circle }> </div></div>
-                                <div className={ style.imageDiv }><img src={ cards.image } alt="" /></div>
-                                <div className={ style.user }>{ cards.user }</div>
-                                <div className={ style.link }>
-                                    <Link to={ cards.linkto }>{ cards.text }</Link>
-                                </div>
-
-                            </div>
-                        )
-
-                    }) }
+  return (
+    <>
+      <div className={style.home}>
+        <h1>Welcome To Clahoot</h1>
+        <div className={style.cardDiv}>
+          {cardItems.map((cards, index) => {
+            return (
+              <div className={style.theCards} key={index} {...cards}>
+                {/* <div className={style.iconDiv}><img src={cards.icon} alt="" /></div> */}
+                <div className={style.iconDiv}>
+                  {" "}
+                  <div className={style.circle}> </div>
                 </div>
-            </div>
-        </>
-
-    )
-}
-export default Home
+                <div className={style.imageDiv}>
+                  <img src={cards.image} alt="" />
+                </div>
+                <div className={style.user}>{cards.user}</div>
+                <div className={style.link}>
+                  <Link to={cards.linkto}>{cards.text}</Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
