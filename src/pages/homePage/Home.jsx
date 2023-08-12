f84eb070aa321852e43d4ce1a9460b198bc64f80
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-12 08:20:00 +0100

    added home link to app.jsx

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,57 +1,55 @@
-import 
+import { Link } from "react-router-dom";
 
 import style from "./Homepage.module.css";
-import logo from "../../assets/Ellipse 12.svg"
-import icon from "../../assets/Group 27.svg"
-import iconTwo from "../../assets/Group 27 (1).svg"
-
+import logo from "../../assets/Ellipse 12.svg";
+import icon from "../../assets/Group 27.svg";
+import iconTwo from "../../assets/Group 27 (1).svg";
 
 const Home = () => {
- let cardItems = [
+  let cardItems = [
     {
-        icon: logo,
-        image: icon,
-        user: "Host",
-        text: "Create a room"
+      icon: logo,
+      image: icon,
+      user: "Host",
+      text: "Create a room",
+      buttonLink: "/",
     },
     {
-        icon: logo,
-        image: iconTwo,
-        user: "Join",
-        text: "Join a room"
-    }
- ]
-
+      icon: logo,
+      image: iconTwo,
+      user: "Join",
+      text: "Join a room",
+      buttonLink: "/gamePin",
+    },
+  ];
 
   return (
     <>
-    <div className={style.App}>
+      <div className={style.App}>
         <h1>Welcome To Clahoot</h1>
         <div className={style.cardDiv}>
-           {cardItems.map((cards, index) => {
+          {cardItems.map((cards, index) => {
             return (
-            <div
-                className={style.theCards}
-                key={index}
-                {...cards}
-            >
-            {/* <div className={style.iconDiv}><img src={cards.icon} alt="" /></div> */}
-            <div className={style.iconDiv}> <div className={style.circle}> </div></div>
-            <div className={style.imageDiv}><img src={cards.image} alt="" /></div>
-            <div className={style.user}>{cards.user}</div>
-            <div className={style.link}>
-                <a href="">{cards.text}</a>
-            </div>
-
-            </div>
-            )
-              
-            })}
+              <div className={style.theCards} key={index} {...cards}>
+                {/* <div className={style.iconDiv}><img src={cards.icon} alt="" /></div> */}
+                <div className={style.iconDiv}>
+                  {" "}
+                  <div className={style.circle}> </div>
+                </div>
+                <div className={style.imageDiv}>
+                  <img src={cards.image} alt="" />
+                </div>
+                <div className={style.user}>{cards.user}</div>
+                <div className={style.link}>
+                  <Link to={cards.buttonLink}>{cards.text}</Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
-    </div>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
