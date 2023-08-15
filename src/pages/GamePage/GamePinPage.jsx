8b93b4160445247e2a86c8bbcb86141315abcd16
oloyewole <oloyewole@keystonebankng.com>
2023-08-15 11:22:51 +0100

    resolved

--- src/pages/GamePage/GamePinPage.jsx
+++ src/pages/GamePage/GamePinPage.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import style from "./gamePin.module.css";
 import CustomButton from "../../components/CustomButton";
+import CustomInput from "../../components/CustomInput/CustomInput";
 
+<<<<<<< HEAD
 const GamePinPage = ({ buttonText }) => {
+=======
+const GamePinPage = () => {
+>>>>>>> 8d55428 (updated repo)
   return (
     <div className={style.header}>
       <h1 className={style.kahoot}>Clahoot!</h1>
       <div className={style.box}>
+<<<<<<< HEAD
         <CustomButton
           style={{ border: "10px solid red" }}
           buttonText={"Activate"}
         />
+=======
+        <CustomInput
+        placeholder={"Game pin"} customStyle={style.kahootInput}
+        
+        />
+        <CustomButton  buttonText={'Activate'} containerStyle={style.kahootBtn}
+        
+        />
+        
+>>>>>>> 8d55428 (updated repo)
       </div>
     </div>
   );
