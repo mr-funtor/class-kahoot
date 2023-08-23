7a9e3604ece93673cd5379fe5715cf73338eb8b2
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-08-23 12:21:55 +0100

    Response tracker updated

--- src/pages/hostSide/responseTracker/ResponseTrackerPage.jsx
+++ src/pages/hostSide/responseTracker/ResponseTrackerPage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import style from "./responseTracker.module.css";
+import CustomButton from "../../../components/CustomButton";
 import options from "../../QuestionPage/options";
 
 const ResponseTrackerPage = () => {
-  return <div>ResponseTrackerPage</div>;
+  function optionMap(item) {
+    return (
+      <CustomButton
+        key={item.id}
+        buttonText={item.value}
+        containerStyle={`${style.default} ${item.backgroundColor}`}
+      />
+    );
+  }
+  return (
+    <div className="pageBody">
+      <div className={style.optionDiv}>{options.map(optionMap)}</div>
+    </div>
+  );
 };
 
 export default ResponseTrackerPage;
