6c631cb32dbefd8181f56285dd6dcde591deb62f
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-08-23 10:57:53 +0100

    Response tracker boilerplate

---
+++ src/pages/hostSide/responseTracker/ResponseTrackerPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./responseTracker.module.css";
+
+const ResponseTrackerPage = () => {
+  return <div>ResponseTrackerPage</div>;
+};
+
+export default ResponseTrackerPage;
