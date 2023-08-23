7ca0366905a56b9e745b2f788501aaa789a877f7
Brown Memoye <memoyebrown@gmail.com>
2023-08-23 10:51:36 +0100

    created HostWaiting page

---
+++ src/pages/hostSide/HostWaiting/index.jsx
@@ -0,0 +1,11 @@
+import styles from './HostWaiting.module.css';
+
+
+const HostWaiting = () => {
+    return (
+        <div>
+            <h1>Get ready to play</h1>
+        </div>
+    )
+}
+export default HostWaiting
