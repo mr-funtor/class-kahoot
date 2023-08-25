0078e250bb302a56e5e8bfe513e31e7411ac0bd8
Brown Memoye <memoyebrown@gmail.com>
2023-08-25 10:01:17 +0100

    working on host waiting room

--- src/pages/hostSide/HostWaiting/index.jsx
+++ src/pages/hostSide/HostWaiting/index.jsx
@@ -1,10 +1,35 @@
+import { BiPlay } from 'react-icons/bi';
+import { CustomButton, LoadingDots } from '../../../components';
 import styles from './HostWaiting.module.css';
 
 
-const HostWaiting = () => {
+const HostWaiting = ({ gamePin, players = [] }) => {
+
+
+
     return (
-        <div className={ styles }>
+        <div className={ styles.HostWaiting }>
             <h1>Get ready to play!</h1>
+            <div className={ styles.gamePinBox }>
+                <h3>Game Pin</h3>
+                <p>{ gamePin ? gamePin
+                    : <span
+                        style={
+                            { display: 'flex', gap: '5px', alignItems: 'center' }
+                        }>
+                        <LoadingDots />
+                    </span> }
+                </p>
+            </div>
+            <CustomButton
+                containerStyle={ `${styles.startBtn}` }
+                buttonText={ <>
+                    <BiPlay className={ styles.playIcon } />
+                    Start!
+                </> }
+                onClick={ () => { console.log("clicked!") } }
+                disabled={ players.length < 2 }
+            />
         </div>
     )
 }
