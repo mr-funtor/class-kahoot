36be6e1ac6afb5dd85398bf981417381b7b8151c
Theophilus Oyeyemi <105336834+MrPhilus@users.noreply.github.com>
2023-08-28 12:46:25 +0100

    barchart added

---
+++ src/pages/hostSide/responseTracker/BarChart.jsx
@@ -0,0 +1,27 @@
+import style from "./responseTracker.module.css";
+
+const BarChart = ({ data }) => {
+  const maxDataValue = Math.max(...data.map((item) => item.point));
+
+  return (
+    <div className={style.barChart}>
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className={style.bar}
+          style={{
+            height: `${(item.point / maxDataValue) * 50}%`,
+            backgroundColor: item.color,
+          }}
+        >
+          <div className={style.labels}>
+            {item.label} {item.point}
+            {item.label === "D -" && <span className={style.checkmark}>✔</span>}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BarChart;
--- src/pages/hostSide/responseTracker/ResponseTrackerPage.jsx
+++ src/pages/hostSide/responseTracker/ResponseTrackerPage.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import style from "./responseTracker.module.css";
 import CustomButton from "../../../components/CustomButton";
 import options from "../../QuestionPage/options";
+import BarChart from "./BarChart";
 
 const ResponseTrackerPage = () => {
+  const data = [
+    { label: "A -", point: 0, color: "hsla(255, 64%, 66%, 1)" },
+    { label: "B -", point: 0, color: "hsla(191, 95%, 46%, 1)" },
+    { label: "C -", point: 0, color: "hsla(47, 79%, 52%, 1)" },
+    { label: "D -", point: 9, color: "hsla(302, 94%, 51%, 1)" },
+  ];
+
   function optionMap(item) {
     return (
       <CustomButton
@@ -14,7 +22,8 @@ const ResponseTrackerPage = () => {
     );
   }
   return (
-    <div className="pageBody">
+    <div className={style.pageBody}>
+      <BarChart data={data} />
       <div className={style.optionDiv}>{options.map(optionMap)}</div>
     </div>
   );
