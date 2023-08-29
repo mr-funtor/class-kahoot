c45ea569ede45894eb3fa17588760cffb35b52d3
initial commit <acpreciousgertrude@gmail.com>
2023-08-29 22:39:47 +0100

    Resolving merging conflicts

--- src/pages/FinalLeaderboard/index.jsx
+++ src/pages/FinalLeaderboard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import styles from "./finalLeaderboard.module.css";
 import {
   img1,
   img2,
@@ -10,15 +10,12 @@ import {
   img8,
   img9,
 } from "../../assets/index";
-import styles from "./finalLeaderboard.module.css";
-import LoadingSpinner from "../../components/LoadingSpinner";
-
 const FinalLeaderboard = () => {
   const players = [
     { name: "Precious", score: 150, image: img1 },
     { name: "Toyin", score: 250, image: img2 },
     { name: "Waliyah", score: 100, image: img3 },
-    { name: "Taiwo", score: 120, image: img4 },
+    { name: "Taiwo", score: 320, image: img4 },
     { name: "Brown", score: 200, image: img5 },
     { name: "Theo", score: 110, image: img6 },
     { name: "Lanre", score: 180, image: img7 },
@@ -27,22 +24,10 @@ const FinalLeaderboard = () => {
   ];
 
   const sortedPlayers = [...players].sort((a, b) => b.score - a.score);
-
-  const firstPlace = {
-    img: sortedPlayers[0].image,
-    name: sortedPlayers[0].name,
-    score: sortedPlayers[0].score,
-  };
-  const secondPlace = {
-    img: sortedPlayers[1].image,
-    name: sortedPlayers[1].name,
-    score: sortedPlayers[1].score,
-  };
-  const thirdPlace = {
-    img: sortedPlayers[2].image,
-    name: sortedPlayers[2].name,
-    score: sortedPlayers[2].score,
-  };
+  // console.log(sortedPlayers[0]);
+  const firstPlace = sortedPlayers[0].name;
+  const secondPlace = sortedPlayers[1].name;
+  const thirdPlace = sortedPlayers[2].name;
 
   return (
     <div className={styles.finalLeaderBoard}>
@@ -78,6 +63,32 @@ const FinalLeaderboard = () => {
           <img className={styles.images} src={thirdPlace.img} alt="" />
           <p>{thirdPlace.name}</p>
           <p>{thirdPlace.score}</p>
+    <div className={styles.positionContainer}>
+      <div className={styles.otherPositions}>
+        <ol start="4">
+          <div>
+            <li>Username (score)</li>
+            <li>Username (score)</li>
+            <li>Username (score)</li>
+          </div>
+          <div>
+            <li>Username (score)</li>
+            <li>Username (score)</li>
+            <li>Username (score)</li>
+          </div>
+        </ol>
+      </div>
+
+      <div className={styles.finalLeaderBoard}>
+        <img
+          style={{ width: "110px" }}
+          src={sortedPlayers[0].image}
+          alt={sortedPlayers[0].name}
+        />
+        <div className={styles.leaderBoard}>
+          <div className={styles.secondPlace}>{firstPlace}</div>
+          <div className={styles.winner}>{secondPlace}</div>
+          <div className={styles.thirdPlace}>{thirdPlace}</div>
         </div>
       </div>
     </div>
