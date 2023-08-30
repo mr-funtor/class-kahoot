b9ae0d53dc68a47b91c00ba1b039b8202edc7568
initial commit <acpreciousgertrude@gmail.com>
2023-08-30 15:18:57 +0100

    Imported and added finalLeaderboard kinks.

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,14 @@ import ResponseTrackerPage from "./pages/hostSide/responseTracker/ResponseTracke
 import HostLeaderBoard from "./pages/hostSide/HostLeaderBoard/HostLeaderBoard";
 import ResultPage from "./pages/hostSide/resultPage/resultPage";
 import WaitingRoom from "./pages/waitingRoom/WaitingRoom";
+import FinalLeaderboard from "./pages/FinalLeaderboard";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <nav
-          style={ {
+          style={{
             width: "100%",
             display: "flex",
             alignItems: "center",
@@ -27,7 +28,7 @@ function App() {
             fontSize: "1.5rem",
             padding: "10px",
             boxShadow: "2px 2px 12px 12px rgba(0,0,0,0.2)",
-          } }
+          }}
         >
           <Link to="/">home</Link>
           <Link to="gamePin">GamePin</Link>
@@ -37,19 +38,21 @@ function App() {
           <Link to="hostwaiting">HostWaiting</Link>
           <Link to="responses">Responses</Link>
           <Link to="result">Result</Link>
+          <Link to="FinalLeaderboard">Final Leaderboard</Link>
         </nav>
         <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/gamePin" element={ <GamePinPage /> } />
-          <Route path="/nickName" element={ <NicknamePage /> } />
-          <Route path="/playerWaiting" element={ <PlayerWaiting /> } />
-          <Route path="/questions" element={ <QuestionPage /> } />
-          <Route path="/responses" element={ <ResponseTrackerPage /> } />
-          <Route path="/result" element={ <ResultPage /> } />
-          <Route path="/hostwaiting" element={ <HostWaiting /> } />
-          <Route path="/leaderBoard" element={ <HostLeaderBoard /> } />
-          <Route path="/waitingRoom" element={ <WaitingRoom /> } />
-          {/* Note: the route is just to see how to navigate to the leaderBoard page, is not necessarily where is meant to be */ }
+          <Route path="/" element={<Home />} />
+          <Route path="/gamePin" element={<GamePinPage />} />
+          <Route path="/nickName" element={<NicknamePage />} />
+          <Route path="/playerWaiting" element={<PlayerWaiting />} />
+          <Route path="/questions" element={<QuestionPage />} />
+          <Route path="/responses" element={<ResponseTrackerPage />} />
+          <Route path="/result" element={<ResultPage />} />
+          <Route path="/hostwaiting" element={<HostWaiting />} />
+          <Route path="/leaderBoard" element={<HostLeaderBoard />} />
+          <Route path="/waitingRoom" element={<WaitingRoom />} />
+          <Route path="/FinalLeaderboard" element={<FinalLeaderboard />} />
+          {/* Note: the route is just to see how to navigate to the leaderBoard page, is not necessarily where is meant to be */}
         </Routes>
       </Router>
     </div>
